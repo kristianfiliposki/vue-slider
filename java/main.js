7699f3a31642a7f65118c528563e796247253d53
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-16 17:28:14 +0100

    1.0

--- java/main.js
+++ java/main.js
@@ -1,21 +1,45 @@
-
 const { createApp } = Vue;
 
 const opzioni = {
     data: function () {
         return {
-            color:`blue`,
-             message: 'Benvenuto ',
-
-        methods:{
-/*             prova: function () {
-                this.message = "funziona";
-                }, */
-            bottoneCliccato: function () {
-                    this.message = "aqua";
+            index:"0",
+            slides : [
+                {
+                    immagine: 'https://img.freepik.com/free-psd/dark-mobile-device-mockup_149660-787.jpg',
+                    nome: 'Smartphone XYZ',
+                    descrizione: 'Uno smartphone potente con caratteristiche avanzate e display nitido.',
+                    prezzo: 499.99
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-psd/laptop-blue-background-mock-up_1022-178.jpg',
+                    nome: 'Laptop ABC',
+                    descrizione: 'Un laptop leggero e performante, ideale per la produttività in movimento.',
+                    prezzo: 899.99
+                },
+                {
+                    immagine: 'https://img.freepik.com/free-photo/photo-camera-balancing-with-yellow-background_23-2150271772.jpg',
+                    nome: 'Fotocamera 123',
+                    descrizione: 'Una fotocamera professionale con alta risoluzione e molte funzionalità.',
+                    prezzo: 699.99
                 }
-            }   
-        }
+            ],
+        }   
+    },
+    methods: {
+        avanti() {
+            let finalindex = this.slides.length-1;
+            if(this.index < finalindex){
+                this.index++;
+            }
+            else if(index = finalindex){
+                this.index=0;
+            }
+        },
+         indietro() {
+            let finalindex = this.slides.length-1;
+             this.index--;
+        },
     }
 };
 createApp(opzioni).mount('#app')
